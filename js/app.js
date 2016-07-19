696a2c65df9d606474e82e04b4cb9d863e3326ea
aungureanu614 <aungureanu614@gmail.com>
2016-07-18 20:08:34 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	
 	$('.convert').on('click', function(){
@@ -6,19 +7,25 @@ $(document).ready(function(){
 	
 	}),
 
+	clear();
+
+});
+
+function clear(){
+	//clear html and texbox
 	$('.clear').on('click', function(){
 		$('.result').empty();
 		$('#userInput').val('');
 	})
-
-	
-});
+}
 
 function checkValue(value){
+	//check if value is a number
 	if(isNaN(value)){
 			alert('Please enter a valid number');
 			
 		}
+	//check if value is a decimal
 	else if(value % 1 != 0){
 			alert('Please enter a non decimal value');
 			
@@ -29,26 +36,35 @@ function checkValue(value){
 		}
 };
 
+
 function fizzbuzz(num){
 
 	for(i=1; i<=num; i++){
+		result = " ";
 		//check if i is divisible by both 3 and 5
-		if((i%3 === 0) && (i%5 === 0)){
-			$('.result').append('FizzBuzz <br>');
+		if(i%15 === 0){
+			result += "FizzBuzz <br>";
 		}
 		//check if i is divisible by 3
 		else if (i%3 === 0) {
-			$('.result').append('Fizz <br>');
+			result += "Fizz <br>";
 		}
 		//check if i is divisible by 5
 		else if (i%5 === 0) {
-			$('.result').append('Buzz <br>');
+			result += "Buzz <br>";
 		}
 		//print i
 		else{
-			$('.result').append(i + '<br>');
+			result += i + '<br>';
 		}
+		appendToResult(result);
+		
 	};
 
-}
+    
+};
 
+function appendToResult(result){
+	//Append result of fizzbuzz to html
+	$('.result').append(result);
+};
